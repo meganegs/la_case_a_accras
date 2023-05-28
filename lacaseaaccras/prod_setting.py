fd477c240068b0e2400748db5ddf4394bb878032
megan <megane.grego@gmail.com>
2023-05-29 01:09:53 +0200

    Add config postgresql

--- lacaseaaccras/prod_setting.py
+++ lacaseaaccras/prod_setting.py
@@ -1,8 +1,11 @@
+import dj_database_url
 from .settings import *
 
 DEBUG = False
 TEMPLATES_DEBUG = False
 SECRET_KEY = 'm8=a5pf5_74zqx^)wz$4h)jk7_1_1w-zna)3b*=!mlmspb*jf)'
 
+DATABASES['default'] = dj_database_url.config()
 
 ALLOWED_HOSTS = ['lacaseaaccras.herokuapp.com', '127.0.0.1']
+
--- lacaseaaccras/settings.py
+++ lacaseaaccras/settings.py
@@ -131,7 +131,6 @@ DATABASES = {
 #    'default': dj_database_url.config()
 #
 #}
-db_from_env = dj_database_url.config(conn_max_age=500)
 #DATABASES['default'].update(db_from_env)
 
 # Password validation
